ce6334c72395aa2e58781775136f0b5f82b98791
Harsh Jha <jharsh995@gmail.com>
2021-02-23 20:04:02 +0530

    Added public private key based transaction signing

--- src/myCoin.js
+++ src/myCoin.js
@@ -1,109 +1,22 @@
-const SHA256 = require('crypto-js/sha256')
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+const {BlockChain, Transaction} = require('./blockChain')
 
-class Block{
-    constructor(timestamp,transactions,previousHash){
-        this.timestamp = timestamp;
-        this.transactions = transactions;
-        this.previousHash = previousHash;
-        this.hash = this.calculateHash();
-        this.counter = 0;
-    }
-
-    calculateHash(){
-        return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash + this.counter).toString();
-    }
-
-    mineBlock(difficulty){
-        while(this.hash.substring(0,difficulty) !== Array(difficulty+1).join("0")){
-            this.counter++;
-            this.hash = this.calculateHash();
-        }
-    }
-
-}
-
-class BlockChain{
-    constructor(){
-        this.chain = [];
-        this.difficulty = 3;
-        this.createGenesis();
-        this.pendingTranctions = [];
-        this.miningReward = 100;
-    }
-
-    createGenesis(){
-        this.chain.push(new Block('18/2/21 17:30','Genesis Block',null));
-    }
-
-    minePendingTransactions(miningRewardAdd){
-        let currDate = Date.now();
-        let block = new Block(currDate,this.pendingTranctions,this.chain[this.chain.length-1].hash);
-        if(this.isChainValid()){
-            block.mineBlock(this.difficulty);
-            console.log('Block successfully mined');
-            this.chain.push(block);
-            this.pendingTranctions = [
-                new Transaction(null,miningRewardAdd,this.miningReward)
-            ];
-        }
-        else{
-            console.log('Chain corrupted!')
-        }
-    }
-
-    createTransaction(transaction){
-        this.pendingTranctions.push(transaction);
-    }
-
-    calculateBalance(address){
-        let balance = 0;
-        for(let block of this.chain){
-            for(let transaction of block.transactions){
-                if(transaction.destAdd == address)
-                    balance += transaction.amount;
-                if(transaction.srcAdd == address)
-                    balance -= transaction.amount;
-            }
-        }
-        return balance;
-    }
-    // addBlock(newBlock){
-    //     if(this.isChainValid()){
-    //         newBlock.previousHash = this.chain[this.chain.length-1].hash;
-    //     }
-    //     newBlock.mineBlock(this.difficulty);
-    //     this.chain.push(newBlock);
-    // }
-
-    isChainValid(){
-        for(let i=1; i< this.chain.length; i++){
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i-1];
-            if(currentBlock.hash !== currentBlock.calculateHash()){
-                return false;
-            }
-            if(currentBlock.previousHash !== previousBlock.hash){
-                return false;
-            }
-        }
-        return true;
-    }
-}
-
-class Transaction{
-    constructor(srcAdd, destAdd, amount){
-        this.srcAdd = srcAdd;
-        this.destAdd = destAdd;
-        this.amount = amount;
-    }
-}
+const myKey = ec.keyFromPrivate('65222979c13fc15b98157d3d5ef12d80b5d1081fce39e98185609421a69139f5');
+const myWalletAdd = myKey.getPublic('hex');
+// console.log(myWalletAdd)
 
 let myBlockChain = new BlockChain();
+const tx1 = new Transaction(myWalletAdd,"Someaddress", 10);
 
-myBlockChain.createTransaction(new Transaction('Jon', 'Brad', 220));
-myBlockChain.createTransaction(new Transaction('Brad', 'Augustus', 20));
-myBlockChain.minePendingTransactions('Harsh');
-console.log(JSON.stringify(myBlockChain));
-myBlockChain.createTransaction(new Transaction('Whitney', 'Augustus', 100));
-myBlockChain.minePendingTransactions('Cooper');
-console.log(JSON.stringify(myBlockChain));
+tx1.signTrans(myKey);
+
+myBlockChain.addTransaction(tx1);
+
+myBlockChain.minePendingTransactions(myWalletAdd);
+
+console.log(myBlockChain.calculateBalance(myWalletAdd));
+
+myBlockChain.minePendingTransactions(myWalletAdd);
+
+console.log(myBlockChain.calculateBalance(myWalletAdd));
